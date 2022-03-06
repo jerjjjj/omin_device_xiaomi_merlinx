a7bf09aac0f60107079a902ed32b388fb27f1283
jerjjj <93431283+jerjjj@users.noreply.github.com>
2022-03-06 16:50:03 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/xiaomi/merlin
+DEVICE_PATH := device/xiaomi/merlinx
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
